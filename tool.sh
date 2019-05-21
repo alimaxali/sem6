173009b215c6dcfdd67d89f0e3c4474c10b6b582
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-05-21 20:52:35 +0200

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -1,5 +1,7 @@
 
+rm -rif /data/data/com.termux/files/home/payload5/.tool/metasploit/payloada.sh
 
 
+cp /data/data/com.termux/files/home/payload5/.tool/sem6/payloada.sh /data/data/com.termux/files/home/payload5/.tool/metasploit
 
-
+ 
